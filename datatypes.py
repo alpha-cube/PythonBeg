476d9d009690fd249d2ebebefb8fd2da3512ebb5
alpha-cube <ameynarvekar74@gmail.com>
2024-07-17 20:45:02 +0530

    push

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+#index
+a=12232
+print(a)
+print("type of a:",type(a))
+
+#float
+c=0.132
+print(c)
+print("type of c:",type(c))
+
+#
+e=complex(2,3)
+print(e)
+print("type of c:",type(e))
+
+#string
+b='arc'
+print(b)
+print("type of b:",type(b))
+
+d=True
+print(d)
+print("type of d:",type(d))
+
