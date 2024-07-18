9238ac69ff5b6772b0c6588224ae81e0fafba735
alpha-cube <ameynarvekar74@gmail.com>
2024-07-18 19:30:15 +0530

    push

---
+++ input.py
@@ -0,0 +1,5 @@
+a=input()
+print(a)
+
+b=input('enter ur name:')
+print('welcome user',b)
---
+++ typecast.py
@@ -0,0 +1,17 @@
+a='1'# this will concat ie it will be a string not a number
+b='2' 
+print (a+b)
+
+#explicit typecsting (modified by us)
+a='1'# string should be valid ie num not names
+b='2' 
+print (int(a)+int(b))
+
+c='5'
+d=2 
+print (int(c)+d)
+
+#implicit typecasting (by itself)
+a1=2
+a2=4.5
+print (a1+a2)# it will be a float
