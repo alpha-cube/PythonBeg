649d7113a19fd4d805d6f946a449fb875a06498c
alpha-cube <ameynarvekar74@gmail.com>
2024-07-18 19:59:05 +0530

    push

--- input.py
+++ input.py
@@ -3,3 +3,8 @@ print(a)
 
 b=input('enter ur name:')
 print('welcome user',b)
+
+a1=int(input())
+a2=float(input())
+print(a1+a2)
+
---
+++ str.py
@@ -0,0 +1,17 @@
+print('''jkshh
+sjkfbsjkfh
+sdjkbsdfb\n'''
+)
+
+#looping (to print all string char)
+a1='''arc
+ane
+711'''
+
+for character in a1:
+    print(character)
+
+a='arcane'
+print(a[2]) #index  o/p 'c'
+
+print(len(a)) #length of string
